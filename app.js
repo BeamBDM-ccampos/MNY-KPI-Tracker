0c74ff29b864ca6ba681974d478267277ef582c9
BeamBDM-ccampos <ccsouthern01@gmail.com>
2026-05-10 22:52:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -206,8 +206,8 @@ async function loadFilteredTrackerData() {
     await wait(350);
     updateLoadingBar(65, "Loading filtered data...");
 
-    await wait(60000);
-    updateLoadingBar(85, "Almost there! Please do not close window...");
+    //await wait(60000);
+    //updateLoadingBar(85, "Almost there! Please do not close window...");
 
     const result = await resultPromise;
 
